9f8c9a3e1bfbe78cec2f747f2e25042daafc5bb9
jarnedel <jarne.delarue@student.howest.be>
2023-09-19 14:52:55 +0200

    feat: frontend init

---
+++ packages/pwa/src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: () => import('../views/Temp.vue'),
+        },
+        {
+          path: '/:pathMatch(.*)*',
+            component: () => import('../views/NotFound.vue'),
+        }
+    ],
+})
+
+router.beforeEach((to, _, next) => {
+    next()
+})
---
+++ packages/pwa/src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    404: page not found
+  </div>
+</template>
+
+<style scoped>
+
+</style>
