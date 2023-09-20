bfa7238ebae9ff0c7c828ebe38722fc7fd23644d
DelfosseDoran <doran.delfosse@student.howest.be>
2023-09-20 15:45:10 +0200

    wrapper auth

--- packages/pwa/src/views/auth/Login.vue
+++ packages/pwa/src/views/auth/Login.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-
+ const email = ref('');
 </script>
 
 <template>
-<div>
-  test
-</div>
+<form>
+  <h1>Login</h1>
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" v-model="email">
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password" v-model="password">
+
+</form>
 </template>
 
 <style scoped>
