b34b3cc65150373f309b1c600227d3578358ad9b
jarnedel <jarne.delarue@student.howest.be>
2023-09-22 15:06:41 +0200

    feat: uno config colors

--- packages/pwa/uno.config.ts
+++ packages/pwa/uno.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
-    // GEEN THEME!
-    // wel manifesteren van genereren css
-});
+  // GEEN THEME!
+  // wel manifesteren van genereren css
+  theme: {
+    colors: {
+      secondary: {
+        DEFAULT: '#F5CB5C',
+        light: '#FAE7B2',
+        lighter: '#FAE8B8',
+        dark: '#F0B719',
+        surface: '#FEFCF6',
+      },
+      primary: {
+        DEFAULT: '#386FA4',
+        light: '#ADD7F6',
+        surface: '#F5F7FA',
+        text: '#0A1004',
+      },
+    },
+  },
+})
