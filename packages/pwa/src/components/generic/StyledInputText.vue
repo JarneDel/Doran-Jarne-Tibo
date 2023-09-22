1d3a7760213286c754ca151b950e4fd58fbb8854
jarnedel <jarne.delarue@student.howest.be>
2023-09-22 16:22:52 +0200

    feat: wip input type text

---
+++ packages/pwa/src/components/generic/StyledInputText.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'StyledInputText',
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
+
+<template>
+  <label>
+    <span>{{ label }}</span>
+    <br />
+    <input
+      :type="type"
+      class="b-2 b-primary-light hover:border-primary focus:border-primary-dark focus-visible:border-primary-dark rounded bg-white px-4 py-1.5 outline-none transition-colors"
+    />
+  </label>
+</template>
+
+<style scoped></style>
--- packages/pwa/src/views/Temp.vue
+++ packages/pwa/src/views/Temp.vue
@@ -2,9 +2,10 @@
 import { defineComponent } from 'vue'
 import StyledButton from '@/components/generic/StyledButton.vue'
 import StyledInternalLink from '@/components/generic/StyledLink.vue'
+import StyledInputText from '@/components/generic/StyledInputText.vue'
 
 export default defineComponent({
-  components: { StyledInternalLink, StyledButton },
+  components: { StyledInputText, StyledInternalLink, StyledButton },
 })
 </script>
 
@@ -13,6 +14,8 @@ export default defineComponent({
   <div>
     text rond <StyledInternalLink to="/">TEST</StyledInternalLink> test achter
   </div>
+  <styled-input-text label="test" type="text" />
+  <input type="date" />
 </template>
 
 <style scoped></style>
