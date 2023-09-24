7bb1f0b6075031d55642652ed4365b5413d73f60
jarnedel <jarne.delarue@student.howest.be>
2023-09-24 20:55:23 +0200

    feat: wrapper and login

--- packages/pwa/src/components/generic/StyledButton.vue
+++ packages/pwa/src/components/generic/StyledButton.vue
@@ -1,16 +1,23 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
 
 export default defineComponent({
   name: 'Button',
+  props: {
+    type: {
+      type: String as PropType<'button' | 'submit' | 'reset'>,
+      default: 'button',
+    },
+  },
   emits: ['click'],
 })
 </script>
 
 <template>
   <button
-    @click="$emit('click')"
+    :type="type"
     class="px4 bg-secondary hover:border-secondary-lighter active:border-secondary-lighter active:bg-secondary-400 focus-visible-outline-none transition-color rounded border-2 border-transparent py-2 focus:border-black focus:outline-none focus-visible:border-black"
+    @click="$emit('click')"
   >
     <slot></slot>
   </button>
