edf46b7f720446c8ebc9cc4d98190b2d41adff3f
TiboMessiaen <tibo.messiaen@student.howest.be>
2023-10-04 17:42:04 +0200

    feat: validation createRoom

--- packages/api/src/room/dto/create-room.input.ts
+++ packages/api/src/room/dto/create-room.input.ts
@@ -1,16 +1,31 @@
-import { InputType, Field, ID } from '@nestjs/graphql'
+import { InputType, Field } from '@nestjs/graphql'
+import { IsString, IsNotEmpty, MinLength, Max, IsIn } from 'class-validator'
+
+const typeList = [
+  'Sportzaal',
+  'Kleedruimte',
+  'Werkruimte',
+  'Zwembad',
+  'Duikput',
+]
 
 @InputType()
 export class CreateRoomInput {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   @Field() // Graphql
   name: string
 
   @Field(() => [String]) // Graphql
   sports: string[]
 
+  @Max(1000)
   @Field({ defaultValue: 0 }) // Graphql
   pricePerHour: number
 
+  @IsNotEmpty()
+  @IsIn(typeList)
   @Field({ defaultValue: 'Sport zaal.' }) // Graphql
   type: string
 }
