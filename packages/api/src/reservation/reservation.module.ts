92825ab47be1c4cae49ab67abab7dc53282e591b
jarnedel <jarne.delarue@student.howest.be>
2023-10-09 17:24:04 +0200

    fix: reservation.module.ts Added TypeOrm and exported ReservationService

--- packages/api/src/reservation/reservation.module.ts
+++ packages/api/src/reservation/reservation.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
-import { ReservationService } from './reservation.service';
-import { ReservationResolver } from './reservation.resolver';
+import { Module } from '@nestjs/common'
+import { ReservationService } from './reservation.service'
+import { ReservationResolver } from './reservation.resolver'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Reservation } from './entities/reservation.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Reservation])],
   providers: [ReservationResolver, ReservationService],
+  exports: [ReservationService],
 })
 export class ReservationModule {}
