a2edea65c3d9fd18cf60f2aba3d73c87e845178e
DelfosseDoran <doran.delfosse@student.howest.be>
2023-10-14 15:05:42 +0200

    feat: wrapper responsive

--- packages/pwa/src/components/wrapper/adminWrapper.vue
+++ packages/pwa/src/components/wrapper/adminWrapper.vue
@@ -10,21 +10,21 @@ export default {
 
 <template>
   <div class="bg-primary-surface flex w-full">
-    <div class="h-screen w-1/4 rounded-r-xl bg-white">
+    <div class="h-screen  rounded-r-xl bg-white">
       <div class="m-8">
-        <div class="flex gap-4">
-          <Logo />
-          <h1 class="text-4xl">{{ $t('navigation.title') }}</h1>
+        <div class="flex xl:gap-4 gap-2 items-center">
+          <Logo class="2xl:h-16 xl:h-8 h-6"/>
+          <h1 class="2xl:text-4xl xl:text-2xl text-xl">{{ $t('navigation.title') }}</h1>
         </div>
         <div class="mt-4 grid gap-2">
           <RouterLink class="flex items-center gap-4" to="/admin/groups">
             <Users />
-            <h2 class="text-2xl">{{ $t('admin.group') }}</h2>
+            <h2 class="xl:text-2xl text-lg">{{ $t('admin.group') }}</h2>
           </RouterLink>
         </div>
       </div>
     </div>
-    <div class="w-3/4">
+    <div class="w-full">
       <RouterView />
     </div>
   </div>
