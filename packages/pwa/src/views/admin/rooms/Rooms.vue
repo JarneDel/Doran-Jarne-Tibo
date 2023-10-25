35e85c37a7a5af27206035d9ec7eca783463d02c
TiboMessiaen <tibo.messiaen@student.howest.be>
2023-10-25 14:09:43 +0200

    fix: room cach problem

--- packages/pwa/src/views/admin/rooms/Rooms.vue
+++ packages/pwa/src/views/admin/rooms/Rooms.vue
@@ -3,88 +3,88 @@
 interface Rooms {
   GetAllRooms: [
     {
-      _id: string;
-      name: string;
-      sports: Sport[];
-      pricePerHour: number;
-      type: string;
-      createdAt: string;
-      updatedAt: string;
-    }
-  ];
+      _id: string
+      name: string
+      sports: Sport[]
+      pricePerHour: number
+      type: string
+      createdAt: string
+      updatedAt: string
+    },
+  ]
   GetAllGyms: [
     {
-      _id: string;
-      name: string;
-      sports: Sport[];
-      pricePerHour: number;
-      type: string;
-      createdAt: string;
-      updatedAt: string;
-    }
-  ];
+      _id: string
+      name: string
+      sports: Sport[]
+      pricePerHour: number
+      type: string
+      createdAt: string
+      updatedAt: string
+    },
+  ]
   GetAllWorkRooms: [
     {
-      _id: string;
-      name: string;
-      sports: Sport[];
-      pricePerHour: number;
-      type: string;
-      createdAt: string;
-      updatedAt: string;
-    }
-  ];
+      _id: string
+      name: string
+      sports: Sport[]
+      pricePerHour: number
+      type: string
+      createdAt: string
+      updatedAt: string
+    },
+  ]
   GetAllChangingRooms: [
     {
-      _id: string;
-      name: string;
-      sports: Sport[];
-      pricePerHour: number;
-      type: string;
-      createdAt: string;
-      updatedAt: string;
-    }
-  ];
+      _id: string
+      name: string
+      sports: Sport[]
+      pricePerHour: number
+      type: string
+      createdAt: string
+      updatedAt: string
+    },
+  ]
   GetAllSwimmingPools: [
     {
-      _id: string;
-      name: string;
-      sports: Sport[];
-      pricePerHour: number;
-      type: string;
-      createdAt: string;
-      updatedAt: string;
-    }
-  ];
+      _id: string
+      name: string
+      sports: Sport[]
+      pricePerHour: number
+      type: string
+      createdAt: string
+      updatedAt: string
+    },
+  ]
   GetAllDivePools: [
     {
-      _id: string;
-      name: string;
-      sports: Sport[];
-      pricePerHour: number;
-      type: string;
-      createdAt: string;
-      updatedAt: string;
-    }
-  ];
+      _id: string
+      name: string
+      sports: Sport[]
+      pricePerHour: number
+      type: string
+      createdAt: string
+      updatedAt: string
+    },
+  ]
 }
 
 interface Sport {
-  id: string;
-  name: string;
+  id: string
+  name: string
 }
 
 // Imports
-import { useQuery } from '@vue/apollo-composable';
+import { useQuery } from '@vue/apollo-composable'
 // import { ALL_ROOMS } from '../../../graphql/room.query';
-import { ALL_GYMS } from '../../../graphql/room.query';
-import { ALL_WORK_ROOMS } from '../../../graphql/room.query';
-import { ALL_CHANGING_ROOMS } from '../../../graphql/room.query';
-import { ALL_SWIMMING_POOLS } from '../../../graphql/room.query';
-import { ALL_DIVE_POOLS } from '../../../graphql/room.query';
-import { defineComponent, ref } from 'vue';
-import UseFirebase from '../../../composables/useFirebase';
-import { PlusCircle } from 'lucide-vue-next';
+import { ALL_GYMS } from '../../../graphql/room.query'
+import { ALL_WORK_ROOMS } from '../../../graphql/room.query'
+import { ALL_CHANGING_ROOMS } from '../../../graphql/room.query'
+import { ALL_SWIMMING_POOLS } from '../../../graphql/room.query'
+import { ALL_DIVE_POOLS } from '../../../graphql/room.query'
+import { defineComponent, ref } from 'vue'
+import UseFirebase from '../../../composables/useFirebase'
+import { PlusCircle } from 'lucide-vue-next'
 
 // Export default
 export default defineComponent({
@@ -92,12 +92,12 @@ export default defineComponent({
     PlusCircle,
   },
   setup() {
-    const { firebaseUser } = UseFirebase();
-    const idToken = ref();
+    const { firebaseUser } = UseFirebase()
+    const idToken = ref()
     const getIdToken = async () => {
-      idToken.value = await firebaseUser.value?.getIdToken();
-    };
-    getIdToken();
+      idToken.value = await firebaseUser.value?.getIdToken()
+    }
+    getIdToken()
     // //All rooms
     // const {
     //   loading,
@@ -108,29 +108,59 @@ export default defineComponent({
       loading: loadingGyms,
       result: resultGyms,
       error: errorGyms,
-    } = useQuery<Rooms>(ALL_GYMS);
+    } = useQuery<Rooms>(
+      ALL_GYMS,
+      {},
+      {
+        fetchPolicy: 'no-cache',
+      },
+    )
     const {
       loading: loadingWorkRooms,
       result: resultWorkRooms,
       error: errorWorkRooms,
-    } = useQuery<Rooms>(ALL_WORK_ROOMS);
+    } = useQuery<Rooms>(
+      ALL_WORK_ROOMS,
+      {},
+      {
+        fetchPolicy: 'no-cache',
+      },
+    )
     const {
       loading: loadingChangingRooms,
       result: resultChangingRooms,
       error: errorChangingRooms,
-    } = useQuery<Rooms>(ALL_CHANGING_ROOMS);
+    } = useQuery<Rooms>(
+      ALL_CHANGING_ROOMS,
+      {},
+      {
+        fetchPolicy: 'no-cache',
+      },
+    )
     const {
       loading: loadingSwimmingPools,
       result: resultSwimmingPools,
       error: errorSwimmingPools,
-    } = useQuery<Rooms>(ALL_SWIMMING_POOLS);
+    } = useQuery<Rooms>(
+      ALL_SWIMMING_POOLS,
+      {},
+      {
+        fetchPolicy: 'no-cache',
+      },
+    )
     const {
       loading: loadingDivePools,
       result: resultDivePools,
       error: errorDivePools,
-    } = useQuery<Rooms>(ALL_DIVE_POOLS);
+    } = useQuery<Rooms>(
+      ALL_DIVE_POOLS,
+      {},
+      {
+        fetchPolicy: 'no-cache',
+      },
+    )
 
-    const TypeSelector = ref(0);
+    const TypeSelector = ref(0)
 
     return {
       idToken,
@@ -153,13 +183,13 @@ export default defineComponent({
       loadingDivePools,
       errorDivePools,
       TypeSelector,
-    };
+    }
   },
-});
+})
 </script>
 
 <template>
-  <div class="flex justify-center items-center">
+  <div class="flex items-center justify-center">
     <button
       @click="TypeSelector = 0"
       :class="{
@@ -213,15 +243,15 @@ export default defineComponent({
   </div>
   <div class="flex flex-col gap-20">
     <div v-if="TypeSelector == 0">
-      <h3 class="font-bold text-3xl mb-2">Sportzalen</h3>
+      <h3 class="mb-2 text-3xl font-bold">Sportzalen</h3>
       <ul class="mx-auto grid grid-cols-3 gap-6">
         <li v-for="gym in resultGyms?.GetAllGyms" :key="gym._id">
           <div class="h-full rounded-lg bg-white p-4 shadow-md">
-            <h3 class="mb-2 font-bold text-2xl">{{ gym.name }}</h3>
+            <h3 class="mb-2 text-2xl font-bold">{{ gym.name }}</h3>
             <p class="text-lg font-semibold">Sports:</p>
             <ul>
               <li
-                class="list-disc ml-4"
+                class="ml-4 list-disc"
                 v-for="sport in resultGyms?.GetAllGyms[
                   resultGyms?.GetAllGyms.indexOf(gym)
                 ].sports"
@@ -239,7 +269,7 @@ export default defineComponent({
         <li>
           <RouterLink
             to="/admin/rooms/create"
-            class="flex w-full justify-center items-center h-full rounded-lg bg-white p-4 shadow-md"
+            class="flex h-full w-full items-center justify-center rounded-lg bg-white p-4 shadow-md"
           >
             <PlusCircle class="text-secondary-light" height="140" width="140" />
           </RouterLink>
@@ -247,20 +277,20 @@ export default defineComponent({
       </ul>
     </div>
     <div v-if="TypeSelector == 1">
-      <h3 class="font-bold text-3xl mb-2">WerkRuimtes</h3>
+      <h3 class="mb-2 text-3xl font-bold">WerkRuimtes</h3>
       <ul class="mx-auto grid grid-cols-3 gap-6">
         <li
           v-for="workRoom in resultWorkRooms?.GetAllWorkRooms"
           :key="workRoom._id"
         >
           <div class="h-full rounded-lg bg-white p-4 shadow-md">
-            <h3 class="mb-2 font-bold text-2xl">{{ workRoom.name }}</h3>
+            <h3 class="mb-2 text-2xl font-bold">{{ workRoom.name }}</h3>
           </div>
         </li>
         <li>
           <RouterLink
             to="/admin/rooms/create"
-            class="flex w-full justify-center items-center h-full rounded-lg bg-white p-4 shadow-md"
+            class="flex h-full w-full items-center justify-center rounded-lg bg-white p-4 shadow-md"
           >
             <PlusCircle class="text-secondary-light" height="140" width="140" />
           </RouterLink>
@@ -268,14 +298,14 @@ export default defineComponent({
       </ul>
     </div>
     <div v-if="TypeSelector == 2">
-      <h3 class="font-bold text-3xl mb-2">Kleedkamers</h3>
+      <h3 class="mb-2 text-3xl font-bold">Kleedkamers</h3>
       <ul class="mx-auto grid grid-cols-3 gap-6">
         <li
           v-for="changingRoom in resultChangingRooms?.GetAllChangingRooms"
           :key="changingRoom._id"
         >
           <div class="h-full rounded-lg bg-white p-4 shadow-md">
-            <h3 class="mb-2 font-bold text-2xl">{{ changingRoom.name }}</h3>
+            <h3 class="mb-2 text-2xl font-bold">{{ changingRoom.name }}</h3>
             <div class="flex items-center gap-1">
               <p class="text-lg font-semibold">Price per hour:</p>
               <p>€{{ changingRoom.pricePerHour }}</p>
@@ -285,7 +315,7 @@ export default defineComponent({
         <li>
           <RouterLink
             to="/admin/rooms/create"
-            class="flex w-full justify-center items-center h-full rounded-lg bg-white p-4 shadow-md"
+            class="flex h-full w-full items-center justify-center rounded-lg bg-white p-4 shadow-md"
           >
             <PlusCircle class="text-secondary-light" height="140" width="140" />
           </RouterLink>
@@ -293,18 +323,18 @@ export default defineComponent({
       </ul>
     </div>
     <div v-if="TypeSelector == 3">
-      <h3 class="font-bold text-3xl mb-2">Zwembaden</h3>
+      <h3 class="mb-2 text-3xl font-bold">Zwembaden</h3>
       <ul class="mx-auto grid grid-cols-3 gap-6">
         <li
           v-for="pool in resultSwimmingPools?.GetAllSwimmingPools"
           :key="pool._id"
         >
           <div class="h-full rounded-lg bg-white p-4 shadow-md">
-            <h3 class="mb-2 font-bold text-2xl">{{ pool.name }}</h3>
+            <h3 class="mb-2 text-2xl font-bold">{{ pool.name }}</h3>
             <p class="text-lg font-semibold">Sports:</p>
             <ul>
               <li
-                class="list-disc ml-4"
+                class="ml-4 list-disc"
                 v-for="sport in resultSwimmingPools?.GetAllSwimmingPools[
                   resultSwimmingPools?.GetAllSwimmingPools.indexOf(pool)
                 ].sports"
@@ -322,7 +352,7 @@ export default defineComponent({
         <li>
           <RouterLink
             to="/admin/rooms/create"
-            class="flex w-full justify-center items-center h-full rounded-lg bg-white p-4 shadow-md"
+            class="flex h-full w-full items-center justify-center rounded-lg bg-white p-4 shadow-md"
           >
             <PlusCircle class="text-secondary-light" height="140" width="140" />
           </RouterLink>
@@ -330,18 +360,18 @@ export default defineComponent({
       </ul>
     </div>
     <div v-if="TypeSelector == 4">
-      <h3 class="font-bold text-3xl mb-2">Duikputten</h3>
+      <h3 class="mb-2 text-3xl font-bold">Duikputten</h3>
       <ul class="mx-auto grid grid-cols-3 gap-6">
         <li
           v-for="divePool in resultDivePools?.GetAllDivePools"
           :key="divePool._id"
         >
           <div class="h-full rounded-lg bg-white p-4 shadow-md">
-            <h3 class="mb-2 font-bold text-2xl">{{ divePool.name }}</h3>
+            <h3 class="mb-2 text-2xl font-bold">{{ divePool.name }}</h3>
             <p class="text-lg font-semibold">Sports:</p>
             <ul>
               <li
-                class="list-disc ml-4"
+                class="ml-4 list-disc"
                 v-for="sport in resultDivePools?.GetAllDivePools[
                   resultDivePools?.GetAllDivePools.indexOf(divePool)
                 ].sports"
@@ -359,7 +389,7 @@ export default defineComponent({
         <li>
           <RouterLink
             to="/admin/rooms/create"
-            class="flex w-full justify-center items-center h-full rounded-lg bg-white p-4 shadow-md"
+            class="flex h-full w-full items-center justify-center rounded-lg bg-white p-4 shadow-md"
           >
             <PlusCircle class="text-secondary-light" height="140" width="140" />
           </RouterLink>
