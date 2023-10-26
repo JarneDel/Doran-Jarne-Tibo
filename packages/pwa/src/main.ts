e8823a082651003ffff20fe799c84f3f3aed80e7
DelfosseDoran <doran.delfosse@student.howest.be>
2023-10-26 14:52:32 +0200

    opkuis van main.ts

--- packages/pwa/src/main.ts
+++ packages/pwa/src/main.ts
@@ -7,12 +7,10 @@ import useFirebase from '@/composables/useFirebase.ts'
 import { i18n } from '@/bootstrap/i18n.ts'
 import { FocusTrap } from 'focus-trap-vue'
 import useCustomUser from '@/composables/useUser.ts'
-import useLanguage from './composables/useLanguage'
 
 
 const { restoreUser, firebaseUser } = useFirebase()
 const { restoreCustomUser, } = useCustomUser()
-// const { setLocale } = useLanguage()
 
 
 
@@ -25,5 +23,4 @@ const { restoreCustomUser, } = useCustomUser()
     .use(i18n)
     .component('FocusTrap', FocusTrap)
     .mount('#app')
-  // createApp(App).use(router).use(i18n).mount('#app')
 })()
