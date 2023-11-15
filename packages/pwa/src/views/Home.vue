6cc75eb548e614f9ad0bc2295c8a20a65c698438
TiboMessiaen <tibo.messiaen@student.howest.be>
2023-11-15 14:53:16 +0100

    foto

--- packages/pwa/src/views/Home.vue
+++ packages/pwa/src/views/Home.vue
@@ -4,42 +4,46 @@ import { type AuthError } from 'firebase/auth'
 
 import useFirebase from '@/composables/useFirebase'
 
-export default{
-    setup(){
-      const { logout } = useFirebase()
+export default {
+  setup() {
+    const { logout } = useFirebase()
 
-      const error = ref<AuthError | null>(null)
-        const handleLogout = () => {
-            logout()
-            .then(() => {
-                console.log('logged out')
-            })
-            .catch((err: AuthError) => {
-                error.value = err
-            })
-        }
-        return {
-            error,
-            handleLogout
-        }
+    const error = ref<AuthError | null>(null)
+    const handleLogout = () => {
+      logout()
+        .then(() => {
+          console.log('logged out')
+        })
+        .catch((err: AuthError) => {
+          error.value = err
+        })
     }
+    return {
+      error,
+      handleLogout,
+    }
+  },
 }
 </script>
 
 <template>
+  <div>
     <div>
-        <div>
-            Locatie
-        </div>
-        <div>
-            Openings uren
-        </div>
-        <div>
-            Sluitings dagen
-        </div>
+      <img
+        class="h-64 w-full object-cover md:h-fit"
+        src="../assets/sportcomplex.webp"
+        alt="Afbeelding van het sportcomplex"
+      />
     </div>
-<button @click="handleLogout" >logout</button>
+    <div
+      class="m-8 flex flex-col items-center justify-center gap-2 md:flex-row"
+    >
+      <div class="w-full bg-green-600">1</div>
+      <div class="w-full bg-green-600">2</div>
+      <div class="w-full bg-green-600">3</div>
+    </div>
+    <div>Sluitings dagen</div>
+  </div>
+  <button @click="handleLogout">logout</button>
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
