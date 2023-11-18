a6cbb430b2f9d0de482e8be61554c621a9ab86c1
JarneDel <jarne.delarue@student.howest.be>
2023-11-18 23:08:12 +0100

    doc: removed copilot q&a

--- packages/pwa/src/layout/Layout.vue
+++ packages/pwa/src/layout/Layout.vue
@@ -1,14 +1,16 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import AppFooter from '@/components/AppFooter.vue'
 import AppHeader from '@/components/AppHeader.vue'
 </script>
 
 <template>
   <div class="">
-    <div class="grid grid-rows-[min-content_1fr] min-h-screen bg-primary-surface">
+    <div
+      class="bg-primary-surface grid min-h-screen grid-rows-[min-content_1fr]"
+    >
       <AppHeader />
       <div class="h-full">
-        <slot/>
+        <slot />
       </div>
     </div>
     <AppFooter />
@@ -16,7 +18,3 @@ import AppHeader from '@/components/AppHeader.vue'
 </template>
 
 <style scoped></style>
-//q: how to make costom grid rows tailwindcss
-//a: https://tailwindcss.com/docs/grid-template-rows
-//q: make me a grid with 2 rows 1 min-content and 1 fr for tailwindcss
-//a: grid-rows-[min-content,fr]
