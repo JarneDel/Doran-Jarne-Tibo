799f18dc661f0abf392d1c16c7824d65c0931999
JarneDel <jarne.delarue@student.howest.be>
2023-11-21 16:31:41 +0100

    Feat: quickfixes

--- packages/pwa/src/views/admin/vacation/VacationOverview.vue
+++ packages/pwa/src/views/admin/vacation/VacationOverview.vue
@@ -131,11 +131,11 @@ export default defineComponent({
       <StyledInputText v-model="rejectMessage" label="Reject message" />
     </template>
     <template v-slot:actions>
-      <StyledButton button-type="danger" @click="rejectVacation">
-        Reject
-      </StyledButton>
       <StyledButton button-type="gray" @click="reject = undefined"
         >Cancel
+      </StyledButton>
+      <StyledButton button-type="danger" @click="rejectVacation">
+        Reject
       </StyledButton>
     </template>
   </Modal>
