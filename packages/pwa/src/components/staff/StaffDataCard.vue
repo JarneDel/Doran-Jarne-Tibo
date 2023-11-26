84da3305fa0aebb3c532b505045ad15e785d5581
jarnedel <jarne.delarue@student.howest.be>
2023-11-26 13:50:29 +0100

    hotfix: staff page alignment and style consistency

--- packages/pwa/src/components/staff/StaffDataCard.vue
+++ packages/pwa/src/components/staff/StaffDataCard.vue
@@ -59,16 +59,6 @@ export default defineComponent({
 </script>
 
 <template>
-  <!--  <div class="b-1 p2 border-black">-->
-  <!--    <h2 class="font-medium">{{ $t('staff.profile') }}</h2>-->
-  <!--    <div>{{ data.lastName + ' ' + data.firstName }}</div>-->
-  <!--    <div>{{ isWorking }}</div>-->
-  <!--    <div>{{ data.email }}</div>-->
-  <!--    <div>{{ data.phone }}</div>-->
-  <!--    <div v-if="isOnVacation">🎉{{ $t('staff.vacation') }}</div>-->
-  <!--    <ProfilePicture editable />-->
-  <!--  </div>-->
-
   <div
     class="h-min w-full overflow-hidden rounded-xl border bg-white shadow-md dark:bg-gray-800"
   >
