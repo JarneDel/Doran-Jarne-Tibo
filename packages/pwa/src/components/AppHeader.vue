b61fecc8c1f5d3fddd4aea4ad950fdb152c63d37
DelfosseDoran <doran.delfosse@student.howest.be>
2023-12-03 01:20:52 +0100

    Refactor AppHeader component

--- packages/pwa/src/components/AppHeader.vue
+++ packages/pwa/src/components/AppHeader.vue
@@ -29,9 +29,7 @@ export default defineComponent({
       push('/logout')
     }
     const toggleOptions = () => {
-      console.log('Toggle Options Called')
       options.value = !options.value
-      console.log('Options:', options.value)
     }
 
     const topNavItems = computed(() => {
@@ -122,21 +120,35 @@ export default defineComponent({
         </label>
       </div>
       <div>
-        <button
-          v-if="customUser"
-          class="gap2 mx-2 flex flex-row items-center justify-center"
-          @click="toggleOptions()"
-        >
-          <span
-            :title="username"
-            class="hidden max-w-[8rem] overflow-hidden text-ellipsis whitespace-nowrap sm:inline-block"
+        <OnClickOutside @trigger="options = false">
+          <button
+            v-if="customUser"
+            class="gap2 mx-2 flex flex-row items-center justify-center"
+            @click="toggleOptions()"
           >
-            {{ username }}
-          </span>
-          <ChevronDown v-if="!options" />
-          <ChevronUp v-else />
-          <ProfilePicture v-if="firebaseUser?.photoURL" :size="48" />
-        </button>
+            <span
+              :title="username"
+              class="hidden max-w-[8rem] overflow-hidden text-ellipsis whitespace-nowrap sm:inline-block"
+            >
+              {{ username }}
+            </span>
+            <ChevronDown v-if="!options" />
+            <ChevronUp v-else />
+            <ProfilePicture v-if="firebaseUser?.photoURL" :size="48" />
+          </button>
+          <div v-if="options">
+            <div
+              class="top-19 z-100 absolute right-4 flex flex-col rounded-md bg-white p-4 shadow-md"
+            >
+              <router-link class="styled-link" to="/profile">{{
+                $t('nav.profile')
+              }}</router-link>
+              <StyledButton class="mt-2" @click="logoutButton()">
+                {{ $t('account.log.out') }}
+              </StyledButton>
+            </div>
+          </div>
+        </OnClickOutside>
         <router-link
           v-if="!customUser"
           class="px4 bg-secondary hover:border-secondary-lighter active:border-secondary-lighter active:bg-secondary-400 focus-visible-outline-none transition-color rounded border-2 border-transparent py-2 focus:border-black focus:outline-none focus-visible:border-black"
@@ -144,18 +156,6 @@ export default defineComponent({
         >
           {{ $t('auth.login') }}
         </router-link>
-        <OnClickOutside v-if="options" @trigger="options = false">
-          <div
-            class="top-19 z-100 absolute right-4 flex flex-col rounded-md bg-white p-4 shadow-md"
-          >
-            <router-link class="styled-link" to="/profile">{{
-              $t('nav.profile')
-            }}</router-link>
-            <StyledButton class="mt-2" @click="logoutButton()">
-              {{ $t('account.log.out') }}
-            </StyledButton>
-          </div>
-        </OnClickOutside>
       </div>
     </div>
   </div>
