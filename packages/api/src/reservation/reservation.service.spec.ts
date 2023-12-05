324caf65f42051b9ece723b246dedb29b8a138ec
DelfosseDoran <doran.delfosse@student.howest.be>
2023-12-05 19:24:45 +0100

    test

--- packages/api/src/reservation/reservation.service.spec.ts
+++ packages/api/src/reservation/reservation.service.spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReservationService } from './reservation.service';
+import { Repository } from 'typeorm'
 
 describe('ReservationService', () => {
   let service: ReservationService;
+  let mockRepository: Repository<Reservation>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
