3c2f9c861e88dd2cc88398c7cfc40f70131ae969
JarneDel <jarne.delarue@student.howest.be>
2023-12-15 18:42:39 +0100

    Add navigation management through useNavigation composable, refractor Sidenav.vue

--- packages/pwa/src/components/layout/Sidenav.vue
+++ packages/pwa/src/components/layout/Sidenav.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { computed, defineComponent, ref, watch } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 import Logo from '@/components/generic/Logo.vue'
 import {
   Bike,
@@ -7,7 +7,6 @@ import {
   CalendarClock,
   Contact2,
   Dumbbell,
-  Icon,
   Palmtree,
   PanelLeftClose,
   PanelRightClose,
@@ -17,7 +16,6 @@ import {
   Wrench,
 } from 'lucide-vue-next'
 import { useRouter } from 'vue-router'
-import { useI18n } from 'vue-i18n'
 import useUser from '@/composables/useUser.ts'
 import { useQuery, useSubscription } from '@vue/apollo-composable'
 import {
@@ -27,15 +25,7 @@ import {
 import useA11y from '@/composables/useA11y.ts'
 import { useWindowSize } from '@vueuse/core'
 import { OnClickOutside } from '@vueuse/components'
-
-interface page {
-  name: string
-  icon: Icon
-  content: string
-  route: string
-  roles: string[]
-  count?: number
-}
+import useNavigation from '@/composables/useNavigation.ts'
 
 export default defineComponent({
   name: 'Sidenav',
@@ -56,122 +46,56 @@ export default defineComponent({
     OnClickOutside,
   },
   setup() {
+    const pendingVacationRequestsCount = ref<number>(0)
+
+    const { customUser } = useUser()
+    const role = computed(() => customUser.value?.role)
+
+    const { currentRoute } = useRouter()
+    const section = computed(() => currentRoute.value.path.split('/')[2])
+
+    const { sidebarIsOpen: isOpen, MOBILE_VIEWPORT_SIZE } = useA11y()
+    const isMobile = computed(() => width.value < MOBILE_VIEWPORT_SIZE.value)
+    const { width } = useWindowSize()
+
+    const { pages: allPages } = useNavigation()
+    const pages = computed(() => {
+      return allPages.value
+        .map(page => {
+          if (page.name === 'vacation') {
+            page.count = pendingVacationRequestsCount.value
+          }
+          return page.roles.includes(role.value ?? '') ? page : null
+        })
+        .filter(Boolean)
+    })
+    // handle subscription and query
     const { result, onResult } = useSubscription(
       VACATION_REQUESTED_SUBSCRIPTION,
     )
-    const { customUser } = useUser()
-
     const { onResult: onInitialResult } = useQuery(VACATION_REQUESTED_COUNT, {
       fetchPolicy: 'cache-and-network',
     })
 
-    const count = ref<number>(0)
-
     onInitialResult(param => {
       console.log('initial result', param)
       if (result.value?.vacationRequested.count) return
-      count.value = param.data?.pendingVacationRequestsCount.count ?? 0
+      pendingVacationRequestsCount.value =
+        param.data?.pendingVacationRequestsCount.count ?? 0
     })
+
     onResult(param => {
       console.log(param)
-      count.value = param.data?.vacationRequested.count ?? 0
+      pendingVacationRequestsCount.value =
+        param.data?.vacationRequested.count ?? 0
     })
 
-    watch(count, () => {
-      console.info(count.value, 'count')
-    })
-
-    const { sidebarIsOpen: isOpen } = useA11y()
-    const { currentRoute } = useRouter()
-    const role = computed(() => customUser.value?.role)
-    const { t } = useI18n()
-    const section = computed(() => currentRoute.value.path.split('/')[2])
-    const pages = computed(() => {
-      const p: page[] = [
-        {
-          name: 'groups',
-          icon: Users,
-          content: t('nav.groups'),
-          route: '/admin/groups',
-          roles: ['ADMIN', 'SUPER_ADMIN', 'STAFF'],
-        },
-        {
-          name: 'inventory',
-          icon: Box,
-          content: t('nav.inventory'),
-          route: '/admin/inventory',
-          roles: ['ADMIN', 'SUPER_ADMIN', 'STAFF'],
-        },
-        {
-          name: 'sport-equipment',
-          icon: Dumbbell,
-          content: t('nav.sportEquipment'),
-          route: '/admin/sport-equipment',
-          roles: ['ADMIN', 'SUPER_ADMIN'],
-        },
-        {
-          name: 'rooms',
-          icon: Warehouse,
-          content: t('nav.rooms'),
-          route: '/admin/rooms',
-          roles: ['ADMIN', 'SUPER_ADMIN', 'STAFF'],
-        },
-        {
-          name: 'reservations',
-          icon: CalendarClock,
-          content: t('nav.reservations'),
-          route: '/admin/reservations',
-          roles: ['ADMIN', 'SUPER_ADMIN', 'STAFF'],
-        },
-        {
-          name: 'repair-requests',
-          icon: Wrench,
-          content: t('nav.repairRequests'),
-          route: '/admin/repair-requests',
-          roles: ['ADMIN', 'SUPER_ADMIN'],
-        },
-        {
-          name: 'staff',
-          icon: Contact2,
-          content: t('nav.staff'),
-          route: '/admin/staff',
-          roles: ['ADMIN', 'SUPER_ADMIN'],
-        },
-        {
-          name: 'vacation',
-          icon: Palmtree,
-          content: t('nav.vacation'),
-          route: '/admin/vacation',
-          roles: ['ADMIN', 'SUPER_ADMIN'],
-          count: count.value,
-        },
-        {
-          name: 'sports',
-          icon: Bike,
-          content: t('nav.sports'),
-          route: '/admin/sports',
-          roles: ['ADMIN', 'SUPER_ADMIN'],
-        },
-        {
-          name: 'services',
-          icon: Tractor,
-          content: t('nav.services'),
-          route: '/admin/services',
-          roles: ['ADMIN', 'SUPER_ADMIN'],
-        },
-      ]
-      return p.filter(page => {
-        return page.roles.includes(role.value ?? '')
-      })
-    })
-
-    const { width } = useWindowSize()
-    const { MOBILE_VIEWPORT_SIZE } = useA11y()
-    const isMobile = computed(() => width.value < MOBILE_VIEWPORT_SIZE.value)
+    // handle click outside
     const clickOutside = () => {
       console.log('click outside')
       if (isMobile.value) isOpen.value = true
     }
+
     return {
       isClosed: isOpen,
       section,
@@ -217,8 +141,8 @@ export default defineComponent({
             'rounded-r-md': section === page.name,
           }"
           :to="page.route"
-          @click="isClosed = true"
           class="px4 relative flex items-center gap-4 py-2"
+          @click="isClosed = true"
         >
           <component :is="page.icon" />
           <h2 v-if="!isClosed" class="font-500">{{ page.content }}</h2>
---
+++ packages/pwa/src/views/admin/AdminOverview.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import useUser from '@/composables/useUser.ts'
+
+export default defineComponent({
+  name: 'AdminOverview',
+  setup() {
+    const { customUser } = useUser()
+
+    return {
+      customUser,
+    }
+  },
+})
+</script>
+
+<template></template>
+
+<style scoped></style>
